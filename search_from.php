a4188e367da04013c09c33ab757ec812fd909eb3
Soham <sohambadheka008@gmail.com>
2014-09-23 02:54:48 +0530

    Create search_from.php

---
+++ search_from.php
@@ -0,0 +1,48 @@
+<p id="searchresults">
+<?php
+	
+	$db = new mysqli('localhost', 'USERNAME', 'PASSWORD', 'DATABASE');
+	
+	if(!$db) {
+		// Show error if we cannot connect.
+		echo 'ERROR: Could not connect to the database.';
+	} else {
+		// Is there a posted query string?
+		if(isset($_POST['queryString'])) {
+			$queryString = $db->real_escape_string($_POST['queryString']);
+			
+			// Is the string length greater than 0?
+			if(strlen($queryString) >0) {
+
+				$query = $db->query("SELECT * FROM wbr_hotels WHERE hot_name LIKE '%" . $queryString . "%' ORDER BY hot_name");
+						
+				if($query) {
+					while ($result = $query ->fetch_object()) {
+						
+	         			echo '<a href="'.$result->hot_website.'">';
+	        
+	         			$name = $result->hot_name;
+	         			if(strlen($name) > 35) { 
+	         				$name = substr($name, 0, 35) . "...";
+	         			}	         			
+	         			echo '<span class="searchheading">'.$name.'</span>';
+	         			
+	         			$description = $result->hot_desc;
+	         			if(strlen($description) > 80) { 
+	         				$description = substr($description, 0, 80) . "...";
+	         			}
+	         			
+	         			echo '<span>'.$description.'</span></a>';
+	         		}
+	         		
+				} else {
+					echo 'ERROR: There was a problem with the query.';
+				}
+			} else {
+				// Dont do anything.
+			} // There is a queryString.
+		} else {
+			echo 'There should be no direct access to this script!';
+		}
+	}
+?>
