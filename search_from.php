ff3257c675cfc1460c91ff06dc3bc453c73594fe
Soham <sohambadheka008@gmail.com>
2015-11-02 15:30:15 -0700

    Updated file with city specific search !

--- search_from.php
+++ search_from.php
@@ -1,7 +1,8 @@
 <p id="searchresults">
 <?php
-	
-	$db = new mysqli('localhost', 'USERNAME', 'PASSWORD', 'DATABASE');
+session_start();	
+$hotcity = $_SESSION['hot_city'];
+	$db = new mysqli('localhost', 'root', '', 'wbr');
 	
 	if(!$db) {
 		// Show error if we cannot connect.
@@ -14,20 +15,20 @@
 			// Is the string length greater than 0?
 			if(strlen($queryString) >0) {
 
-				$query = $db->query("SELECT * FROM wbr_hotels WHERE hot_name LIKE '%" . $queryString . "%' ORDER BY hot_name");
+				$query = $db->query("SELECT * FROM wbr_hotels WHERE hot_city = '$hotcity' AND hot_name LIKE '%".$queryString."%'");
 						
 				if($query) {
 					while ($result = $query ->fetch_object()) {
 						
 	         			echo '<a href="'.$result->hot_website.'">';
-	        
+	         			
 	         			$name = $result->hot_name;
 	         			if(strlen($name) > 35) { 
 	         				$name = substr($name, 0, 35) . "...";
 	         			}	         			
 	         			echo '<span class="searchheading">'.$name.'</span>';
 	         			
-	         			$description = $result->hot_desc;
+	         			$description = $result->hot_website;
 	         			if(strlen($description) > 80) { 
 	         				$description = substr($description, 0, 80) . "...";
 	         			}
